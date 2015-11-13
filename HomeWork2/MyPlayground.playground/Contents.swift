46a05cd03f5aaccd855fc5660cd2bfeeaeba4356
Cristina Blanco <cris@macbook-de-garnik-harutyunyan.local>
2015-11-13 22:39:52 +0100

    prueba1

---
+++ HomeWork2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var i = 0 ; i < 101 ; i++ {
+    
+    if (i % 5 == 0) {
+        print("\(i)Bingo!!!")
+    }
+    
+    if ( i % 2 == 0) {
+        print("\(i)par")
+    } else {
+        print("\(i)impar")
+    }
+    
+    if (i >= 30 && i <= 40) {
+        print("\(i)Viva Swift!!!")
+    }
+}
