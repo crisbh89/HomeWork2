4a36f172b2d5580cd038754e23ec20d11cf07677
Cristina Blanco <cris@macbook-de-garnik-harutyunyan.local>
2015-11-14 20:02:49 +0100

    Valida1

--- HomeWork2/Homework2.playground/Contents.swift
+++ HomeWork2/Homework2.playground/Contents.swift
@@ -5,16 +5,16 @@ import UIKit
 for var i = 0 ; i < 101 ; i++ {
     
     if (i % 5 == 0) {
-        print("\(i)Bingo!!!")
+        print("# \(i)Bingo!!!")
     }
     
     if ( i % 2 == 0) {
-        print("\(i)par")
+        print("# \(i)par")
     } else {
-        print("\(i)impar")
+        print("# \(i)impar")
     }
     
     if (i >= 30 && i <= 40) {
-        print("\(i)Viva Swift!!!")
+        print("# \(i)Viva Swift!!!")
     }
 }
